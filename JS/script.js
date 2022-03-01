c38f3be4cf5b6ba2697f582826027791a952bf1f
Barat-Tojiyev <barattojiyev@gmail.com>
2022-03-01 14:59:25 +0500

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,53 @@
+var ism='Suxrob'
+var familya='Oblayorov'
+var ismFamilya=ism+' '+familya
+console.log(ismFamilya)
+
+var ism='Suxrob'
+var familya='Oblayorov'
+var ismFamilya=ism-familya
+console.log(ismFamilya)
+
+var son1=5
+var son2=15 
+var natija=son1+son2
+console.log(natija)
+
+
+var son1=25
+var son2=15 
+var natija=son1-son2
+console.log(natija)
+
+
+var son1=5
+var son2=15 
+var natija=son1*son2
+console.log(natija)
+
+
+var son1=5
+var son2=15 
+var natija=son1/son2
+console.log(natija)
+
+
+var son1=5
+var son2=15 
+var natija=son1**3
+console.log(natija)
+
+
+var son1=5
+var son2=15
+son1++
+son1++
+son2--
+console.log(son1,son2)
+
+
+
+var son1=5
+var son2=15 
+var natija=son2%4
+console.log(natija)
